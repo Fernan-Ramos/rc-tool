a1466164e71465ea5066614f3873eed3a19f55fe
Fernan-Ramos <ferburgos93@gmail.com>
2021-03-21 11:11:10 +0100

    Refactor

--- create_component.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CURRENT_PATH=`PWD`
-echo "${CURRENT_PATH}"
-mkdir -p ./$1
-touch ./$1/styles.jsx
-NODE_MODULES_PATH=`npm root -g`
-sed -e "s/COMPONENT_NAME/$1/g" "${NODE_MODULES_PATH}/react-cli/code_templates/component.jsx" > ./$1/index.jsx
